2f8da8abdb147b06e03978a2be5c7c4ba8db7e6a
David Raznick <kindly@gmail.com>
2021-12-31 02:03:59 +0000

    start ui

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+		<v-expansion-panels v-model="panel">
+			<v-expansion-panel>
+				<v-expansion-panel-header>
+					Upload
+				</v-expansion-panel-header>
+				
+				<v-expansion-panel-content>
+					<v-form ref="upload-form">
+						<v-file-input
+							v-model="fileUpload"	
+							label="File input"
+							id="file-input"
+							name="file"
+							outlined
+							dense
+							required
+						></v-file-input>
+						<v-btn
+							color="success"
+							@click="upload">
+							Upload
+						</v-btn>
+					</v-form>
+				</v-expansion-panel-content>
+			</v-expansion-panel>
+			<v-expansion-panel>
+				<v-expansion-panel-header>
+					Link
+				</v-expansion-panel-header>
+				<v-expansion-panel-content>
+					Link
+				</v-expansion-panel-content>
+			</v-expansion-panel>
+			<v-expansion-panel>
+				<v-expansion-panel-header>
+					Paste
+				</v-expansion-panel-header>
+				<v-expansion-panel-content>
+					Paste
+				</v-expansion-panel-content>
+			</v-expansion-panel>
+		</v-expansion-panels>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  export default {
+    name: 'Home',
+    data () {
+      return {
+				panel: 0,
+				fileUpload: null,
+				fetchResponse: null,
+				fetchErorr: null
+      }
+    },
+		methods: {
+      upload () {
+				console.log(this.fileUpload) 
+				let formData = new FormData();
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+				formData.append( 'file', this.fileUpload );
+
+				fetch('/api/convert?output_format=preview', {
+						method: 'POST',
+						headers: {
+								'Accept': 'application/json',
+						},
+						body: formData
+				} )
+				.then( function( response ){
+						if( response.status != 201 ){
+								this.fetchError = response.status;
+						}else{
+								response.json().then( function( data ){
+										this.fetchResponse = data;
+										console.log(data);
+								}.bind(this));
+						}
+				}.bind(this));
+      },
+		}
   }
-}
 </script>
